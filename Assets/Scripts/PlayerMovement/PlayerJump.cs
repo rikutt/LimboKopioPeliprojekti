96d4aa168a5f91b48a596003b9b88a4c64678fa9
rikutt <115475887+rikutt@users.noreply.github.com>
2023-01-31 13:13:05 +0200

    Lisää pikkufixejä playermovementtiin ja sumutesti

--- Assets/Scripts/PlayerMovement/PlayerJump.cs
+++ Assets/Scripts/PlayerMovement/PlayerJump.cs
@@ -19,17 +19,16 @@ namespace Barebones2D.Movement
     {
         [SerializeField] private PlayerManager PlayerInstance;
 
-        [SerializeField]
-        private float jumpForce, wallJumpForce,
+        [SerializeField] private float jumpForce, wallJumpForce,
                          jumpDelay, coyoteTime,
                          gravityScalerMaxTime;
 
         [SerializeField] private int doubleJumpAmount;
         private int doubleJumpCounter;
 
-        private float coyoteCheckTimer, jumpDelayTimer, gravityScalerTimer;
-
         private bool hasLetGoOfJump;
+
+        private float coyoteCheckTimer, jumpDelayTimer, gravityScalerTimer;
 
         Vector2 JumpVelocity;
 
@@ -51,19 +50,9 @@ namespace Barebones2D.Movement
                 coyoteCheckTimer = coyoteTime;
             }
 
-            // Have to release jump key to jump again
-            
-
-        }
-        private void FixedUpdate()
-        {
-            if (PlayerInstance.JumpButtonValue > 0)
-                Jumping();
-            else
-                hasLetGoOfJump = true;
-
             GravityChangeOnJumpHold();
         }
+
         void GravityChangeOnJumpHold()
         {
             if (PlayerInstance.JumpButtonValue > 0 && gravityScalerTimer > 0)
@@ -74,6 +63,15 @@ namespace Barebones2D.Movement
                 PlayerInstance.Rigidbody2D.gravityScale = 3f;
 
         }
+        private void FixedUpdate()
+        {
+            if (PlayerInstance.JumpButtonValue > 0)
+                Jumping();
+
+            else
+                hasLetGoOfJump = true;
+        }
+
         void Jumping()
         {
             if (PlayerInstance.IsDodging || jumpDelayTimer > 0 || !hasLetGoOfJump)
@@ -81,81 +79,27 @@ namespace Barebones2D.Movement
 
             // Normal / walljumps / no jump (normal jump maintains velocity.x)
             if (PlayerInstance.IsTouchingRightWall && !PlayerInstance.IsGrounded)
-                JumpVelocity = new Vector2(-1.0f, 0.5f).normalized * wallJumpForce;
+                JumpVelocity = new Vector2(-1.0f, 0.6f).normalized * wallJumpForce;
 
             else if (PlayerInstance.IsTouchingLeftWall && !PlayerInstance.IsGrounded)
-                JumpVelocity = new Vector2(1.0f, 0.5f).normalized * wallJumpForce;
+                JumpVelocity = new Vector2(1.0f, 0.6f).normalized * wallJumpForce;
 
             else if (PlayerInstance.IsGrounded || coyoteCheckTimer > 0 || doubleJumpCounter > 0)
-                JumpVelocity = Vector2.up * new Vector2(PlayerInstance.Rigidbody2D.velocity.x, jumpForce);
+                JumpVelocity = new Vector2(PlayerInstance.Rigidbody2D.velocity.x, jumpForce);
 
             else return;
-
+            
             PlayerInstance.Rigidbody2D.velocity = JumpVelocity;
 
-            jumpDelayTimer = jumpDelay;
-            gravityScalerTimer = gravityScalerMaxTime;
             hasLetGoOfJump = false;
             --doubleJumpCounter;
+
+            // timers reset
+            jumpDelayTimer = jumpDelay;
+            gravityScalerTimer = gravityScalerMaxTime;
 
             // remove coyote jump
             coyoteCheckTimer -= coyoteTime;
         }
-
-        /*void Jumping()
-        {
-            if (PlayerInstance.JumpButtonValue > 0 && _timerFromLastJump < 0 && hasLetGoOfJump && !PlayerInstance.IsDashing)
-            {
-                if (PlayerInstance.IsGrounded || _coyoteTimerCheck > 0 || _doubleJumpCounter > 0 && !PlayerInstance.IsTouchingLeftWall && !PlayerInstance.IsTouchingRightWall)
-                {
-                    Vector2 velocitySwitch = PlayerInstance.Rigidbody2D.velocity;
-                    velocitySwitch.y = jumpForce;
-                    PlayerInstance.Rigidbody2D.velocity = velocitySwitch;
-
-                    _timerFromLastJump = jumpDelayValue;
-                    gravityScalerTimer = gravityJumpScalerTime;
-                    _coyoteTimerCheck -= coyoteTime;
-
-                    hasLetGoOfJump = false;
-                    --_doubleJumpCounter;
-                }
-            }
-            // wall jumps! could need a facing direction comparison
-            // LEFT WALL!!!!
-            if (PlayerInstance.JumpButtonValue > 0 && _timerFromLastJump < 0 && hasLetGoOfJump && !PlayerInstance.IsDashing && !PlayerInstance.IsGrounded && PlayerInstance.IsTouchingLeftWall)
-            {
-                PlayerInstance.Rigidbody2D.velocity = wallJumpDirectionToRight * wallJumpForce;
-
-                _timerFromLastJump = jumpDelayValue;
-                gravityScalerTimer = gravityJumpScalerTime;
-
-                hasLetGoOfJump = false;
-            }
-            // RIGHT WALL!!! en jaksa refaktoroida näitä yheks metodiks
-            if (PlayerInstance.JumpButtonValue > 0 && _timerFromLastJump < 0 && hasLetGoOfJump && !PlayerInstance.IsDashing && !PlayerInstance.IsGrounded && PlayerInstance.IsTouchingRightWall)
-            {
-                PlayerInstance.Rigidbody2D.velocity = wallJumpDirectionToLeft * wallJumpForce;
-
-                _timerFromLastJump = jumpDelayValue;
-                gravityScalerTimer = gravityJumpScalerTime;
-
-                hasLetGoOfJump = false;
-            }
-
-            // no continuous jumping
-            if (PlayerInstance.JumpButtonValue == 0) hasLetGoOfJump = true;
-
-            // decrease gravity when jumping
-            if (PlayerInstance.JumpButtonValue > 0 && gravityScalerTimer > 0)
-            {
-                PlayerInstance.Rigidbody2D.gravityScale = 0.4f;
-            }
-            // faster fall
-            else if (PlayerInstance.JumpButtonValue == 0 || gravityScalerTimer < 0)
-            {
-                PlayerInstance.Rigidbody2D.gravityScale = 3f;
-            }
-        }*/
-
     }
 }
--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 // https://github.com/Matthew-J-Spencer/Ultimate-2D-Controller/blob/main/Scripts/PlayerController.cs
 // https://github.com/Dawnosaur/platformer-movement/blob/main/Scripts/PlayerMovement.cs
 
+// has horizontal movement and Clamped fall speed next to walls and not
+// TODO air movement speed
+
 namespace Barebones2D.Movement
 {
     public class PlayerMovement : MonoBehaviour
     {
 
-        public PlayerManager PlayerInstance;
+        [SerializeField] private PlayerManager PlayerInstance;
 
-        [SerializeField] private float maxSpeed, _accelerationAmount, 
-                                       _decelerationAmount, maxFallSpeed, 
+        [SerializeField] private float maxSpeed, accelerationSpeed, 
+                                       decelerationSpeed, maxFallSpeed, 
                                        maxFallWhileTouchingWalls;
+
+        [SerializeField] private float airAcceleration, airDeceleration;
+        private void Update()
+        {
+            
+        }
 
         void FixedUpdate()
         {
             HorizontalMovement();
             ClampFallSpeedAndWallslide();
-
         }
 
         void HorizontalMovement()
         {
             if (PlayerInstance.IsDodging) return;
 
+            float accelerationAmount;
+            float decelerationAmount;
+
+            if (PlayerInstance.IsGrounded)
+            {
+                accelerationAmount = accelerationSpeed;
+                decelerationAmount = decelerationSpeed;
+            }
+            else
+            {
+                accelerationAmount = airAcceleration;
+                decelerationAmount = airDeceleration;
+            }
+                
+
             float targetSpeed = PlayerInstance.MovementDirectionVector2.x * maxSpeed;
+            
 
             //when to accelerate/decelerate
-            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? _accelerationAmount : _decelerationAmount;
+            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? accelerationAmount : decelerationAmount;
 
             // conserve momentum when moving faster than target
-            if (Mathf.Abs(PlayerInstance.Rigidbody2D.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(PlayerInstance.Rigidbody2D.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && !PlayerInstance.IsGrounded)
+            if (Mathf.Abs(PlayerInstance.Rigidbody2D.velocity.x) > Mathf.Abs(targetSpeed) 
+                && Mathf.Sign(PlayerInstance.Rigidbody2D.velocity.x) == Mathf.Sign(targetSpeed) 
+                && Mathf.Abs(targetSpeed) > 0.01f && !PlayerInstance.IsGrounded)
             {
                 accelRate = 0;
             }
-
-             // if(Data.doConserveMomentum && Mathf.Abs(RB.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(RB.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
 
             float speedDiff = targetSpeed - PlayerInstance.Rigidbody2D.velocity.x;
             float movement = speedDiff * accelRate;
@@ -45,7 +69,7 @@ namespace Barebones2D.Movement
             PlayerInstance.Rigidbody2D.AddForce(movement * Vector2.right, ForceMode2D.Force);
         }
 
-
+        // fall speed
         void ClampFallSpeedAndWallslide()
         {
             if (PlayerInstance.IsDodging) return;
